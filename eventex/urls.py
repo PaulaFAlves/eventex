11537f29405fc575296b7ba8d062c484fb656ecc
Paula Alves <paulafernandaalves@icloud.com>
2019-11-14 11:51:30 -0200

    Rotas 3

--- eventex/urls.py
+++ eventex/urls.py
@@ -14,14 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from eventex.core.views import home
-from eventex.subscription.views import subscribe, detail
+
 
 urlpatterns = [
     path('', home, name='home'),
-    path('inscricao/', subscribe),
-    path('inscricao/<int:pk>/', detail),
+    path('', include('eventex.subscription.urls')),
     path('admin/', admin.site.urls),
 ]
