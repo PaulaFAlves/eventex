e28325572641b46839ab5f85775721a3550e7cb4
Paula Alves <paulafernandaalves@icloud.com>
2019-11-18 11:51:31 -0200

    Email e telefone opcionais

--- eventex/subscription/tests/test_view_new.py
+++ eventex/subscription/tests/test_view_new.py
@@ -78,5 +78,14 @@ class SubscriptionNewPostInvalid(TestCase):
 		self.assertFalse(Subscription.objects.exists())
 
 
+class TemplateRegressionTest(TestCase):
+	def test_template_has_non_field_errors(self):
+		invalid_data = dict(name='Henrique Bastos', cpf='12345678901')
+		response = self.client.post(r('subscription:new'), invalid_data)
+
+		self.assertContains(response, '<ul class="errorlist nonfield">')
+		
+
+
 
 
